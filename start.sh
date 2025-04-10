a0969dab41c5d5004a932e514494103b29d6c1c0
Felix Koppe <fkoppe@web.de>
2025-04-10 12:03:23 +0200

    Name container

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ mkdir -p output
 
 echo "🗣️ Running script.py inside Docker container..."
 
-docker run --rm \
+docker run --name coqui \
   -v coqui_cache:/root/.local/share/tts \
   -v "$(pwd)/output":/app/output \
   -v "$(pwd)/script.py":/app/script.py \
